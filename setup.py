2e3f4daf442792cc844487c1f327b4dec973ee9b
kasper.souren <kasper.souren@0554d5a5-2f33-0410-a3cc-4d58a51ba057>
2007-07-18 16:27:23 +0000

    * first draft of setup.py * minor edit of INSTALL.txt

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import pydot
+
+setup(	name = 'trust-metrics',
+	version = pydot.__version__,
+	description = 'Analyse trust metrics on social networks',
+	author = 'Kasper Souren',
+	author_email = 'souren@itc.it',
+	url = 'http://trustlet.org/wiki/Code',
+	license = 'aGPLv3',
+	platforms = ["any"],
+	classifiers =	[
+                   #     'Development Status :: 5 - Production/Stable',	\
+                         'Intended Audience :: Science/Research',	\
+                         'License :: OSI Approved :: GPL License',\
+                         'Natural Language :: English',			\
+                         'Operating System :: OS Independent',		\
+                         'Programming Language :: Python',		\
+                   #     'Topic :: Scientific/Engineering :: Visualization',\
+                         'Topic :: Software Development :: Libraries :: Python Modules'],
+	long_description = "\n".join(pydot.__doc__.split('\n')),
+	py_modules = ['advogato'])
