129f879c779432269d90a1c9d017a76c6c084bf2
kasper.souren <kasper.souren@0554d5a5-2f33-0410-a3cc-4d58a51ba057>
2007-07-19 16:19:44 +0000

    which trust metrics to implement, copied from wiki

--- trustlet/TrustMetric.py
+++ trustlet/TrustMetric.py
@@ -1,4 +1,13 @@
 
-class TrustMetric:
+'''
+to implement:
+-- advogato trust metric (or use the one available from site
+-- pagerank (find adaptation)
+-- moletrust (the trivial one I proposed, I can do this)
+-- ebay like (simple unpersonalized average)
+-- predict always "master"
+-- more?
+'''                          
 
+class TrustMetric:
     pass
