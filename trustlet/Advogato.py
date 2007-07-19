91f38d5f6ed74a054eb75f3d58f8ed3a36c1efb3
kasper.souren <kasper.souren@0554d5a5-2f33-0410-a3cc-4d58a51ba057>
2007-07-19 11:28:19 +0000

    Nicer seperation between Dataset and Advogato class

--- trustlet/Advogato.py
+++ trustlet/Advogato.py
@@ -3,27 +3,11 @@
 import Dataset
 
 class Advogato(Dataset.Network):
-
     def load_network(self):
-        self.download_dotfile()
-
-    def download_dotfile(self):
-        url = "http://www.advogato.org/person/graph.dot"
-        import os
-        path = os.path
-        if os.environ.has_key('HOME'):
-            home = os.environ['HOME']
-        dataset_path = path.join(home, "datasets")
-        if not os.path.exists(dataset_path):
-            os.mkdir(dataset_path)
-        adv_path = path.join(dataset_path, 'advogato')
-        if not os.path.exists(adv_path):
-            os.mkdir(adv_path)
-        import urllib
-        p = urllib.urlretrieve(url, path.join(adv_path, 'graph.dot'))
-
+        self.download("http://www.advogato.org/person/graph.dot", 'graph.dot')
+        
 
 if __name__ == "__main__":
-    a = Advogato()
-    a.load_network()
+    adv = Advogato()
+    adv.load_network()
             
--- trustlet/Dataset.py
+++ trustlet/Dataset.py
@@ -1,5 +1,22 @@
 
+import os
+
 class Network:
-    def load_network(self):
-        pass
-    
+    def __init__(self):
+        self.path = os.path.join(self.dataset_dir(), self.__class__.__name__)
+        if not os.path.exists(self.path):
+            os.mkdir(self.path)
+
+    def dataset_dir(self):
+        if os.environ.has_key('HOME'):
+            home = os.environ['HOME']
+        self.dataset_path = os.path.join(home, 'datasets')
+        if not os.path.exists(self.dataset_path):
+            os.mkdir(self.dataset_path)
+        return self.dataset_path
+        
+    def download(self, url, file):
+        filepath = os.path.join(self.path, file)
+        print "Downloading %s to %s " % (url, filepath)
+        import urllib
+        p = urllib.urlretrieve(url, filepath)
