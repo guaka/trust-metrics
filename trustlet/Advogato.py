0f4b673d3981aa6e801b83ef3fce299fe0b8fa7d
paolo.massa <paolo.massa@0554d5a5-2f33-0410-a3cc-4d58a51ba057>
2007-07-23 13:54:14 +0000

    Better dealing with file/path stuff (for test)

--- trustlet/Advogato.py
+++ trustlet/Advogato.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
 import Dataset
+import os
 
 class Advogato(Dataset.Network):
     def __init__(self):
         Dataset.Network.__init__(self)
         self.url = "http://www.advogato.org/person/graph.dot"
         self.file = 'graph.dot'
-    
+        self.filepath = os.path.join(self.path, self.file)
+        
     def download(self):
         self.download(self.url, self.file)
 
