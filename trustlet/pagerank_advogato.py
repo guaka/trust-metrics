b67137e6d38a46cd7701d51d262bc487f1525d12
paolo.massa <paolo.massa@0554d5a5-2f33-0410-a3cc-4d58a51ba057>
2007-08-20 19:48:04 +0000

    Added some more tests. We really need to understand if networkx is fast enough for complex computations on large graphs.

--- trustlet/pagerank_advogato.py
+++ trustlet/pagerank_advogato.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python
 
-# import networkxfuture.page_rank as NX_future_page_rank
 import page_rank as NX_future_page_rank
 import networkx
 import sys
 
-# import numpy  # currently not used 
-
 import trustlet.Advogato
 a = trustlet.Advogato.Advogato()
 
-print "Starting reading the Advogato dataset"
+print "Starting reading the dataset"
 
-G = networkx.read_dot(a.filepath)
-G.ban_multiedges() #pagerank doesn't work on multiedge graphs
+#G=networkx.generators.random_graphs.erdos_renyi_graph(10,0.15)
+#G = networkx.read_dot(a.filepath)
+G = networkx.read_dot("/home/phauly/datasets/Advogato/graph03.dot")
+print "network is of type="+type(G)
+G=networkx.DiGraph(G) #cast a XDiGraph into a Graph, maybe this is not needed for pagerank to work, maybe it is needed
+#G.ban_multiedges() #pagerank doesn't work on multiedge graphs
 
-print "Finished reading the Advogato dataset --> Number of nodes of dataset = %s" % len(G.nodes())
+print "Finished reading the dataset --> Number of nodes of dataset = %s" % len(G.nodes())
+
+#print G.edges()
 
 print "Do you want to run numpy.linalg.eig? (y/n) [It might be very heavy]"
 answer=sys.stdin.readline()[:-1]
 if answer == "y":
+    import numpy
     M=NX_future_page_rank.google_matrix(G,alpha=0.9)
     e,ev=numpy.linalg.eig(M.T)
     p=numpy.array(ev[:,0]/ev[:,0].sum())[:,0]
