1dfd80f0d877d04c6ca055ba01bb75188606e2b9
paolo.massa <paolo.massa@0554d5a5-2f33-0410-a3cc-4d58a51ba057>
2007-08-20 16:13:54 +0000

    Cleaned pagerank_advogato.py and added questions before each method so that you can skip heavy methods which will crash your machine

--- trustlet/pagerank_advogato.py
+++ trustlet/pagerank_advogato.py
@@ -3,38 +3,42 @@
 # import networkxfuture.page_rank as NX_future_page_rank
 import page_rank as NX_future_page_rank
 import networkx
+import sys
 
 # import numpy  # currently not used 
 
 import trustlet.Advogato
 a = trustlet.Advogato.Advogato()
 
-def test_graph():
-    G = networkx.DiGraph()
-    edges=[(1,2),(1,3),\
-           (3,1),(3,2),(3,5),\
-           (4,5),(4,6),\
-           (5,4),(5,6),\
-           (6,4)]    
-    G.add_edges_from(edges)
-    return G
+print "Starting reading the Advogato dataset"
 
 G = networkx.read_dot(a.filepath)
-# H = G.copy() #possibility
 G.ban_multiedges() #pagerank doesn't work on multiedge graphs
 
-print "--> Numero nodi=%s" % len(G.nodes())
+print "Finished reading the Advogato dataset --> Number of nodes of dataset = %s" % len(G.nodes())
 
-#M=my_page_rank.google_matrix(G,alpha=0.9)
-#e,ev=numpy.linalg.eig(M.T)
-#p=numpy.array(ev[:,0]/ev[:,0].sum())[:,0]
-#print "exact  ", p
+print "Do you want to run numpy.linalg.eig? (y/n) [It might be very heavy]"
+answer=sys.stdin.readline()[:-1]
+if answer == "y":
+    M=NX_future_page_rank.google_matrix(G,alpha=0.9)
+    e,ev=numpy.linalg.eig(M.T)
+    p=numpy.array(ev[:,0]/ev[:,0].sum())[:,0]
+    print "exact  ", p
 
-pr = NX_future_page_rank.page_rank(G,alpha=0.9,tol=1.0e-8)
-print "networkx", pr.values()
+print "Do you want to run page_rank? (y/n) [It might be very heavy]"
+answer=sys.stdin.readline()[:-1]
+if answer == "y":
+    pr = NX_future_page_rank.page_rank(G,alpha=0.9,tol=1.0e-8)
+    print "networkx", pr.values()
 
-#np=my_page_rank.page_rank_numpy(G,alpha=0.9)
-#print "numpy  ", np
+print "Do you want to run page_rank numpy? (y/n) [It might be very heavy]"
+answer=sys.stdin.readline()[:-1]
+if answer == "y":
+    np=NX_future_page_rank.page_rank_numpy(G,alpha=0.9)
+    print "numpy  ", np
 
-#ns=my_page_rank.page_rank_scipy(G,alpha=0.9)
-#print "scipy  ", ns
+print "Do you want to run page_rank scipy? (y/n) [It might be very heavy]"
+answer=sys.stdin.readline()[:-1]
+if answer == "y":
+    ns=NX_future_page_rank.page_rank_scipy(G,alpha=0.9)
+    print "scipy  ", ns
