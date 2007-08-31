7f20dbc368c6d14869c116b94a97a5cb142a1811
kasper.souren <kasper.souren@0554d5a5-2f33-0410-a3cc-4d58a51ba057>
2007-08-31 00:16:01 +0000

    Moved to functional trust metrics, with lambda and such We should subclass NetworkX for this to become very pretty!

--- trustlet/evaluate.py
+++ trustlet/evaluate.py
@@ -1,76 +1,98 @@
 from trustlet.Advogato import get_graph_dot, Advogato
-import random
-
-class Random_tm:
-    def predict_trust(self,graph,a,b):
-        return random.random()
-
-class Advogato_global_tm:
-    pass
-
-class Advogato_local_tm:
-    pass
-
-class Pagerank_tm:
-    pass
-
-class Ebay_tm:
-    #predicted_trust(a,b)=average incoming links(b) # a does not matter
-    pass
-
-class Kasper_tm:
-    #average outgoing links of a
-    ## check the Jesus/Gandhi paradox: Jesus/Gandhi were good but too trustful, i.e. they were trusting also bad people and so do you if you trust their judgements.
-    pass
-
-class Zero_tm:
-    pass
-
-class One_tm:
-    pass
-
-class MoleTrust_tm:
-    pass
 
 
-def evaluate(Tm,graph):
-    #error_graph=graph.get_nodes() # same nodes, no edges
+def evaluate(trustmetric, graph):
+    #error_graph = graph.get_nodes() # same nodes, no edges
 
     abs_error = 0
     not_predicted_edges = 0
 
-    tm=Tm() # clever to pass the graph here, just once the TM is instantiated? 
+    # tm=TrustMetric() # clever to pass the graph here, just once the TM is instantiated? 
 
+    i = 0
     #Attention. Edges that are self loops might be problematic
     for edge in graph.edges():
-        print "edge=",edge,
+        #print "edge=",edge,
         graph.delete_edge(edge)
 
-        #check if passing the graph all the time is not clever
-        predicted_trust=tm.predict_trust(graph,edge[0],edge[1])
+        predicted_trust = trustmetric(graph, edge[0], edge[1])
 
         if predicted_trust == None:
             not_predicted_edges += 1
         else:
             #add predicted_trust as the value on edge (A,B)
-            abs_error += abs( predicted_trust - float(edge[2]['level']))
+            abs_error += abs(predicted_trust - float(edge[2]['level']))
 
-        print "error=",abs_error," real=",
-        
+            """
+            Maybe it's also interesting to keep track of an error
+            value where too much trust is worse than not trusting
+            enough?
+            """
+            
+        i += 1
+        if divmod(i, 10000)[1] == 0:
+            print "acc=",abs_error / i #," real=",
         graph.add_edge(edge)
 
     print "Error=",abs_error
 
     num_edges = graph.number_of_edges()
     coverage = (num_edges - not_predicted_edges) / num_edges
-    accuracy = abs_error / num_edges
+    print "accuracy=",abs_error / num_edges
 
-import networkx
+# in ipython use:
+# reload(evaluate); evaluate.evaluate(Random_tm, graph)
 
-print "loading..."
-#graph = get_graph_dot()
-a = Advogato()
-graph = networkx.read_dot(a.numbersfilepath)
-print "end loading..."
-evaluate(Random_tm,graph)
+
+
+def outa_tm(G, a, b):
+    #average outgoing links of a
+    trust_list = map(lambda x: float(x[2]['level']), G.edges(a))
+    if trust_list:
+        return sum(trust_list) / len(trust_list)
+    else:
+        return 0
+
+def outb_tm(G, a, b):
+    #average outgoing links of b
+    trust_list = map(lambda x: float(x[2]['level']), G.edges(b))
+    if trust_list:
+        return sum(trust_list) / len(trust_list)
+    else:
+        return 0
+
+## check the Jesus/Gandhi paradox: Jesus/Gandhi were good but too trustful, i.e. they were trusting also bad people and so do you if you trust their judgements.
+
+
+def MoleTrust_tm():
+    pass
+
+def advogato_global_tm(graph, a, b):
+    pass
+
+def advogato_local_tm(graph, a, b):
+    pass
+
+class Pagerank_tm:
+    pass
+
+def ebay_tm(G, a, b):
+    #predicted_trust(a,b)=average incoming links(b) # a does not matter
+    pass
+
+
+
+if __name__ == "__main__":
+    import networkx
+    a = Advogato()
+    graph = networkx.read_dot(a.numbersfilepath)
+
+    import random
+    evaluate(lambda G,a,b: random.random(), graph)
+    evaluate(lambda G,a,b: 0, graph)
+    evaluate(lambda G,a,b: 0.6, graph)
+    evaluate(lambda G,a,b: 0.8, graph)
+    evaluate(lambda G,a,b: 0.9, graph)
+    evaluate(outa_tm, graph)  # this is the best from these "funny" trust metrics
+    evaluate(outb_tm, graph)
 
