8ee2d398d5bcb7f194c9df94fe4ca6586d663a0c
paolo.massa <paolo.massa@0554d5a5-2f33-0410-a3cc-4d58a51ba057>
2007-12-21 14:28:34 +0000

    Some more methods to test

--- examples/dataset_all_methods.py
+++ examples/dataset_all_methods.py
@@ -18,11 +18,17 @@ datasets = [D,U]
 def call_all_methods(N):
     print "#--------------------------------------------"
     print "# Network: ",N.__name__
+    print "get_number_of_nodes()="+N.get_number_of_nodes()
+    print "get_number_of_edges()="+N.get_number_of_edges()
     print "is_directed()="+N.is_directed()
     print "avg_in_degree()="+N.avg_in_degree()
     print "avg_out_degree()="+N.avg_out_degree()
     print "stddev_in_degree()="+N.avg_in_degree()
     print "stddev_out_degree()="+N.avg_out_degree()
+    print "clustering()="+N.clustering()
+    print "get_number_nodes_in_strongly_connected_component()="+N.get_number_nodes_in_strongly_connected_component()
+    print "get_number_nodes_in_connected_component()="+N.get_number_nodes_in_connected_component()
+    print "avg_distance()="+N.get_number_nodes_in_connected_component()
     print ".... more in a second"
 
 for network in networks:
