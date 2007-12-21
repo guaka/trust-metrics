a037732bd998afd1ab0ae2adb09cb3f5f7326cdf
paolo.massa <paolo.massa@0554d5a5-2f33-0410-a3cc-4d58a51ba057>
2007-12-21 15:51:42 +0000

    a stub for testing trust metrics

---
+++ examples/test_trust_metrics.py
@@ -0,0 +1,24 @@
+"""
+Create networks, create trust metrics, test trust metrics on these datasets (also restricting to a certain percentage of edges)
+"""
+
+# make sure example can be run from examples/ directory
+import sys
+sys.path.append('../trustlet')
+
+from Dataset.Advogato import *
+from Dataset.Dummy import DummyNetwork
+
+#create datasets
+dummyNetwork=DummyNetwork()
+
+#create trust metrics
+EbayTM=EbayTM()
+MT2=Moletrust(2)
+
+#create the predgraphs based on leave-one-out (also with ratios)
+pred_graph=PredGrap(dummyNetwork,MT2,0.1) #predict 10% of edges with leave-one-out
+
+#show evaluation measures
+predgraph.showTable()
+predgraph.MAE()
