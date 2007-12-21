5a4926de74944fa0b0409d5ca578ca5752879b43
paolo.massa <paolo.massa@0554d5a5-2f33-0410-a3cc-4d58a51ba057>
2007-12-21 15:34:45 +0000

    Added creation of old advogato dataset (2004) in which on the edges there is "green, blue, violet" instead of "journeyer, master, ..."

--- examples/advogato-dataset.py
+++ examples/advogato-dataset.py
@@ -28,6 +28,11 @@ A.info()
 
 # create Advogato dataset as it was on a certain date.
 # The .dot file is taken from http://www.trustlet.org/datasets/advogato/ looking for the correctly dated file
-AD = AdvogatoNetwork(2007,10,13)
+AD = AdvogatoNetwork(date="2007-10-13")
 AD.info() 
+
+# create Advogato dataset as it was on a certain date.
+# The .dot file is taken from http://www.trustlet.org/datasets/advogato/ looking for the correctly dated file
+AOD = AdvogatoNetwork(date="2004-10-28") # the format of this file is with "violet, green, blue" on the edges and it is different from the current one with "journeyer, master, ..."
+AOD.info()
 
