3fff7d6ab160ea082be39944ebc2ef3809fc0ed9
paolo.massa <paolo.massa@0554d5a5-2f33-0410-a3cc-4d58a51ba057>
2007-12-28 17:43:39 +0000

    one more test

--- examples/test_trust_metrics.py
+++ examples/test_trust_metrics.py
@@ -20,3 +20,16 @@ pred_graph = PredGraph(MoleTrustTM(dummy_network, horizon = 2, threshold = 0.5),
 # show evaluation measures
 pred_graph.abs_error()
 # pred_graph.show_table()
+
+
+# create trust metrics
+# create the predgraphs based on leave-one-out (also with ratios)
+# predict 10% of edges with leave-one-out
+pred_graph = PredGraph(MoleTrustTM(dummy_network, horizon = 2, threshold = 0.5),
+                       leave_one_out=False,
+                       # predict_ratio = 0.5
+                       )
+
+# show evaluation measures
+pred_graph.abs_error()
+# pred_graph.show_table()
