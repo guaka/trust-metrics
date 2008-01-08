c607047c9aa632c6b3ca4c67dd8bef7152375357
kasper.souren <kasper.souren@0554d5a5-2f33-0410-a3cc-4d58a51ba057>
2008-01-08 13:42:13 +0000

    * fix filename * more readable * there's a segfault...

--- examples/network_read_from_file.py
+++ examples/network_read_from_file.py
@@ -2,22 +2,19 @@
 Create networks reading data from files, in different formats (dot, pajek, graphml, ...)
 """
 
-# make sure example can be run from examples/ directory
-import sys
-sys.path.append('../trustlet')
+from trustlet import *
 
-from Dataset import *
 
 #create a undirected unweighted network from a dot file 
-UDUW=read_dot("data/undirected_unweighted.dot")
+UDUW = read_dot("data/undirected_unweighted.dot")
 UDUW.info()
 
 #create a undirected unweighted network from a dot file 
-ErdosUDUW=read_dot("data/erdos971_undirected_unweighted.dot")
+ErdosUDUW = read_dot("data/erdos971_undirected_unweighted.net")
 ErdosUDUW.info()
 
 #create a directed weighted network from a dot file 
-RagusaDW=read_dot("data/ragusa16_directed_weighted.net")
+RagusaDW = read_dot("data/ragusa16_directed_weighted.net")
 RagusaDW.info()
 
 graphML = read_GraphML('simple-graphml.xml')
