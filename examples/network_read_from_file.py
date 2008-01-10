fad9bd238078d60d7c50f930161b634b83315ce8
kasper.souren <kasper.souren@0554d5a5-2f33-0410-a3cc-4d58a51ba057>
2008-01-10 12:07:10 +0000

    slightly fixing

--- examples/network_read_from_file.py
+++ examples/network_read_from_file.py
@@ -5,17 +5,24 @@ Create networks reading data from files, in different formats (dot, pajek, graph
 from trustlet import *
 
 
+graphML = read_graphml('data/simple-graphml.xml')
+graphML.info()
+
+
 #create a undirected unweighted network from a dot file 
 UDUW = read_dot("data/undirected_unweighted.dot")
 UDUW.info()
 
+''' why?
+ syntax error in line 10 near '*'
+ Segmentation fault
+''' 
+
 #create a undirected unweighted network from a dot file 
-ErdosUDUW = read_dot("data/erdos971_undirected_unweighted.net")
-ErdosUDUW.info()
+#ErdosUDUW = read_dot("data/erdos971_undirected_unweighted.net")
+#ErdosUDUW.info()
 
 #create a directed weighted network from a dot file 
-RagusaDW = read_dot("data/ragusa16_directed_weighted.net")
-RagusaDW.info()
+#RagusaDW = read_dot("data/ragusa16_directed_weighted.net")
+#RagusaDW.info()
 
-graphML = read_GraphML('simple-graphml.xml')
-graphML.info()
