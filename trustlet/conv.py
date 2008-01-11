b3e89f3b7ce574e0d906f8cf62c2ac28e5b0d6ab
kasper.souren <kasper.souren@0554d5a5-2f33-0410-a3cc-4d58a51ba057>
2008-01-11 13:33:57 +0000

    * wrapper for NX read functions * using wrapper, and thus read support for 13 new formats or so

--- trustlet/conv.py
+++ trustlet/conv.py
@@ -15,24 +15,31 @@ def read_pajek(filename):
 
     G = Network.Network()
     netconv.netconv2NX(n, G)
-
     return G
 
 
-def read_dot(filename):
-    """Read dot."""
+def _nx_read_wrap(func):
+    """Wrapper around networkx file read functions."""
+    def nx_reader(filename):
+        G = NX.__getattribute__(func)(filename)
+        N = Network.Network()
+        N.paste_graph(G)
+        return N
+    return nx_reader
 
-    G = NX.read_dot(filename)
-    N = Network.Network()
-    N.paste_graph(G)
-    return N
+# there should be a nicer way:
 
+read_adjlist = _nx_read_wrap("read_adjlist")
+read_dot = _nx_read_wrap("read_dot")
+read_edgelist = _nx_read_wrap("read_edgelist")
+read_gml = _nx_read_wrap("read_gml")
+read_gpickle = _nx_read_wrap("read_gpickle")
+read_graph6 = _nx_read_wrap("read_graph6")
+read_graph6_list = _nx_read_wrap("read_graph6_list")
+read_graphml = _nx_read_wrap("read_graphml")
+read_leda = _nx_read_wrap("read_leda")
+read_mulitline_adjlist = _nx_read_wrap("read_multiline_adjlist")
+read_sparse6 = _nx_read_wrap("read_sparse6")
+read_sparse6_list = _nx_read_wrap("read_sparse6_list")
+read_yaml = _nx_read_wrap("read_yaml")
 
-def read_graphml(filename):
-    """Read graphML."""
-
-    G = NX.read_graphml(filename)
-    N = Network.Network()
-    N.paste_graph(G)
-    return N
-    
