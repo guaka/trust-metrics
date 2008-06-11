60560a344f90ae05651cbe03808a9ccb53e5b515
ciropom <ciropom@0554d5a5-2f33-0410-a3cc-4d58a51ba057>
2008-06-11 17:49:33 +0000

    fixed script

--- scripts/printAllTrustMetricsPerformance.py
+++ scripts/printAllTrustMetricsPerformance.py
@@ -13,7 +13,15 @@ for tm in tmlist:
 pointlist = []
 
 for p in plist:
-    print get_name(p.TM)
-    pointlist.append( p.graphcontroversiality( 0.3 , 0.01, toe="mae", np=2 ) )
+    pointlist.append( ( get_name(p.TM) , p.graphcontroversiality( 0.3 , 0.01, toe="mae", np=2 ) ) )
 
-prettyplot( pointlist, "/home/ciropom/AllTrustMetric.png", legend=tuple( [tm for tm in tmlist] ) )
+for p in pointlist:
+    for q in pointlist:
+        if q[0] <= p[0]:
+            continue
+        else:
+            print q[0]+"_vs_"+p[0]
+            prettyplot( [q[1],p[1]], 
+                        "/home/ciropom/graphs/"+q[0]+"_vs_"+p[0]+".png", 
+                        legend=(q[0],p[0]), 
+                        showlines=True )
