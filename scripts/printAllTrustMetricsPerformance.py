2a58b605bf0f849dda951e0afc729ee1bb3bd28c
ciropom <ciropom@0554d5a5-2f33-0410-a3cc-4d58a51ba057>
2008-06-12 08:15:09 +0000

    improved script

--- scripts/printAllTrustMetricsPerformance.py
+++ scripts/printAllTrustMetricsPerformance.py
@@ -1,27 +1,56 @@
+"""
+This script calculate the graphics in gnuplot that compares pair of network.
+If there aren't parameters, it take all the trust metric, elsewhere you can
+specify on command line space-separated name of trustmetric to leave out.
+"""
+
+
 from trustlet import *
 
-A = AdvogatoNetwork( date="2008-05-12" )
 
-tmlist = getTrustMetrics( A )
-del tmlist["PageRankTM"]
+def compareAllTrustMetrics( leaveOut ):
+    
+    A = AdvogatoNetwork( date="2008-05-12" )
 
-plist = []
+    tmlist = getTrustMetrics( A )
+    
+    for l in leaveOut:
+        del tmlist[l]
 
-for tm in tmlist:
-    plist.append( PredGraph( tmlist[tm] ) )
+    plist = []
+    
+    for tm in tmlist:
+        plist.append( PredGraph( tmlist[tm] ) )
 
-pointlist = []
+    pointlist = []
 
-for p in plist:
-    pointlist.append( ( get_name(p.TM) , p.graphcontroversiality( 0.3 , 0.01, toe="mae", np=2 ) ) )
+    for p in plist:
+        pointlist.append( ( get_name(p.TM) , p.graphcontroversiality( 0.3 , 0.01, toe="mae", np=2 ) ) )
 
-for p in pointlist:
-    for q in pointlist:
-        if q[0] <= p[0]:
-            continue
-        else:
-            print q[0]+"_vs_"+p[0]
-            prettyplot( [q[1],p[1]], 
-                        "/home/ciropom/graphs/"+q[0]+"_vs_"+p[0]+".png", 
-                        legend=(q[0],p[0]), 
-                        showlines=True )
+    for p in pointlist:
+        for q in pointlist:
+            if q[0] <= p[0]:
+                continue
+            else:
+                print q[0]+"_vs_"+p[0]
+                prettyplot( [q[1],p[1]], 
+                            "/home/ciropom/graphs/"+q[0]+"_vs_"+p[0]+".png", 
+                            legend=(q[0],p[0]), 
+                            showlines=True )
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) > 1:
+        
+        nleaveOut = len(sys.argv) - 1
+        leaveOut = []
+        for i in xrange(1,nleaveOut):
+            leaveOut.append( sys.argv[i] )
+
+        compareAllTrustMetrics( leaveOut )
+    
+    else:
+    
+        compareAllTrustMetrics( [] )
