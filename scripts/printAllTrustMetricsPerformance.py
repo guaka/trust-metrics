40b8ba9fa71bfc317f2663f2c58d3c153494ed8e
ciropom <ciropom@0554d5a5-2f33-0410-a3cc-4d58a51ba057>
2008-06-17 09:50:18 +0000

    changed TODO

--- scripts/printAllTrustMetricsPerformance.py
+++ scripts/printAllTrustMetricsPerformance.py
@@ -12,7 +12,7 @@ def compareAllTrustMetrics( leaveOut = [],
                             cond=None,date = "2008-05-12", allInOne=True, 
                             path = "/home/ciropom/graphs", toe = "mae", np=1,
                             x_range=(0.0,0.4),
-                            y_range=None, ind=10 ):
+                            y_range=(0.0,1.1), ind=10 ):
     """
     toe can have all possible values PredGraph.graphcontroversiality function, 
     and a special value "all" that indicates that you would calculate 4 graphs 
