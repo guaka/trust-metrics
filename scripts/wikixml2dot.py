a154b7a1eae34d9a56577bd06048e5f7b182b5b8
ciropom <ciropom@0554d5a5-2f33-0410-a3cc-4d58a51ba057>
2008-06-30 17:55:55 +0000

    added getCollaborators function

--- scripts/wikixml2dot.py
+++ scripts/wikixml2dot.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from xml import sax
+from string import index
 
 def main():
     #parser = sax.make_parser()
@@ -24,6 +25,72 @@ class WikiContentHandler(sax.handler.ContentHandler):
         self.level -= 1
     def characters(self,contents):
         print str(contents)
+
+
+def getCollaborators( name, rawWikiText ):
+    """
+    return a list of tuple with ( user, value ), where user is the name of user
+    that put a message on the page, and the value is the number of times that
+    he appear in rawText passed.
+    parameter:
+       name: name of user 
+       rawWikiText: text in wiki format (normally discussion in wiki)
+    """
+
+    resname = []
+
+    exit = 0; start = 0; search = "User:"; io = 5
+
+    #try user, if there aren't, try Utente (italian)
+    while exit < 2:
+
+        try:
+            iu = index( rawWikiText, search, start )
+        except ValueError:
+            exit += 1
+            start = 0
+            io = 7
+            search = "Utente:"
+            if exit >= 2:
+                continue
+
+        start += iu + io
+        end = index( rawWikiText, "|", start ) 
+        username = rawWikiText[start:end]
+        resname.append( username )
+        start += end + 1 #not consider the |
+    
+    #return a list of tuple, the second value of tuple is the weight    
+    return weight( resname )
+
+
+def weight( list ):
+    listweight = []
+    tmp = list
+
+    def update( list, val ):
+        find = False
+
+        for x in xrange(len(list)):
+            if list[x][0] == val:
+                find = True
+                break
+
+        if find:
+            new = list[x][1] + 1
+            del list[x]
+            list.append( (val,new) )
+        else:
+            list.append( (val,1) )
+
+        return
+
+    for x in tmp:
+        update( listweight, x )
+
+    return listweight
+
+
 
 if __name__=="__main__":
     main()
