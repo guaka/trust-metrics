1232b2b2c5e5ee277cb096f1e7b742249e839eff
ciropom <ciropom@0554d5a5-2f33-0410-a3cc-4d58a51ba057>
2008-07-04 09:34:31 +0000

    improved genpredgraph

--- trustlet/genpredgraph.py
+++ trustlet/genpredgraph.py
@@ -20,6 +20,14 @@ def main(net):
     return None
 
 
+def check( path ):
+    from os.path import split
+
+    if split( path )[0] == path:
+        return False
+
+    return True
+
 if __name__ == "__main__":
     import sys,re
     from os.path import split
@@ -28,31 +36,37 @@ if __name__ == "__main__":
 
     if l > 1:
         dataset_path = sys.argv[1]
-        dataset = dataset_path
-        type = split( dataset )[1]
+        type_path = date_path = dataset_path
+        type = date = split( dataset_path )[1]
     else:
-        print "USAGE: python genpredgraph.py dataset_path date [lang]"
+        print "USAGE: python genpredgraph.py dataset_path [lang]"
 
     #try to find type of network
-    while( re.match( "[a-zA-Z]+Network", type ) == None ):
-        dataset,type = split(dataset)
+    while( check(type_path) and ( re.match( "[a-zA-Z]+Network", type ) == None ) ):
+        type_path,type = split(type_path)
+
+    #try to find date
+    while( check(date_path) and ( re.match( "[0-9]{4}-[0-9]{2}-[0-9]{2}", date ) == None ) ):
+        date_path,date = split(date_path)
+
+    if not check(date_path):
+        print "I cannot be able to find date of network in this path!"
+        exit(0)
+
+    if not check(type_path):
+        print "I cannot be able to find type of network in this path!"
+        exit(0)
 
     if type == "WikiNetwork":
-        if l > 3:
-            lang = sys.argv[3]
-            date = sys.argv[2]
+        if l > 2:
+            lang = sys.argv[2]
             A = WikiNetwork( lang,date )
         else:
-            print "you must specify lang and date for wikinetwork!"
+            print "WikiNetwork must have lang!"
             exit(0)
     elif type == "AdvogatoNetwork":
-        if l > 2:
-            date = sys.argv[2]
-            A = AdvogatoNetwork( date=date )
-        else:
-            print "you must specify date for advogatonetwork!"
-            exit(0)
-
+        A = AdvogatoNetwork( date=date )
+        
     if  A == None:
         print "I can't be able to open this dataset"
         exit(0)
