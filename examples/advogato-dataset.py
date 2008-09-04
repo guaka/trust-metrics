6247e0dfc899fc958a09cb9b353a39338705f480
ciropom <ciropom@0554d5a5-2f33-0410-a3cc-4d58a51ba057>
2008-09-04 16:44:02 +0000

    now all examples scripts works

--- examples/advogato-dataset.py
+++ examples/advogato-dataset.py
@@ -10,7 +10,7 @@ D = DummyNetwork()
 D.info() # show information
 
 # create Kaitiaki dataset
-K = KaitiakiNetwork(download = True)
+K = KaitiakiNetwork(download = True, date='2008-09-01')
 K.info() # show information
 
 # create SqueakFoundation dataset
@@ -23,11 +23,14 @@ A.info()
 
 # create Advogato dataset as it was on a certain date.
 # The .dot file is taken from http://www.trustlet.org/datasets/advogato/ looking for the correctly dated file
-AD = AdvogatoNetwork(date="2007-10-13")
+
+
+AD = AdvogatoNetwork(date="2007-10-13", download=True)
 AD.info() 
 
 # create Advogato dataset as it was on a certain date.
 # The .dot file is taken from http://www.trustlet.org/datasets/advogato/ looking for the correctly dated file
-AOD = AdvogatoNetwork(date="2004-10-28") # the format of this file is with "violet, green, blue" on the edges and it is different from the current one with "journeyer, master, ..."
+
+AOD = AdvogatoNetwork(date="2004-10-28",download=True) # the format of this file is with "violet, green, blue" on the edges and it is different from the current one with "journeyer, master, ..."
 AOD.info()
 
