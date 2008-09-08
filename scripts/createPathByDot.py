717936947e6249c38c2d27cc31f7289473cedc9a
ciropom <ciropom@0554d5a5-2f33-0410-a3cc-4d58a51ba057>
2008-09-08 12:16:32 +0000

    bug fix

--- scripts/createPathByDot.py
+++ scripts/createPathByDot.py
@@ -4,11 +4,13 @@ This script take a list of dot files (or c2 files) that contains a graph
 and create a default path with the information stored in the name of the file.
 ex:
 robots_net-graph-2008-07-24.dot --> robots_net/2008-07-24/graph.dot
+
+You can use this script to automatially create a default structure for the datasets folder
 """
 from trustlet.helpers import mkpath
 from shutil import move
 from os import remove,listdir
-from os.path import join,sep
+from os.path import join,split,sep,isdir
 import re
 
 def main( graphList ):
@@ -17,12 +19,18 @@ def main( graphList ):
     rename = re.compile( "[a-z_]+" )
 
     for graph in graphList:
+        # check
+        if isdir( graph ) or '.dot' not in graph:
+            continue
+
         print "processing", graph
 
-        date = redate.findall( graph )[0]
-        name = rename.findall( graph )[0]
+        path,graphname = split( graph )
+
+        date = redate.findall( graphname )[0]
+        name = rename.findall( graphname )[0]
         
-        dest = join( "." , name , date )
+        dest = join( path , name , date )
         mkpath( dest )
         move( graph, join( dest, "graph.dot" ) )
         
