b2a9e95fa4b265984abea5740c54a89075c992d5
the9ull <the9ull@0554d5a5-2f33-0410-a3cc-4d58a51ba057>
2008-10-30 15:57:32 +0000

    added multifile erase

--- scripts/syncrm.py
+++ scripts/syncrm.py
@@ -16,25 +16,31 @@ HOME = os.environ['HOME']
 def main(args):
     if not args:
         return
-    
-    p = path.abspath(args[0])
 
-    if not p.startswith(path.join(HOME,'datasets')):
-        print 'Path to remove have to be in ~/datasets dir'
-        return
+    ps = []
+ 
+    for p in args:
+        p = path.abspath(p)
 
-    if path.isfile(p):
-        os.remove(p)
-    elif path.isdir(p):
-        shutil.rmtree(p)
-    else:
-        assert 0,'File %s doesn\'t exists' % p
+        if not p.startswith(path.join(HOME,'datasets')):
+            print 'Path to remove have to be in ~/datasets dir'
+            print '(%s)'%p
+            return
 
-    # path to hidden path
-    p = p.replace(path.join(HOME,'datasets'),path.join(HOME,'.datasets'))
+        if path.isfile(p):
+            os.remove(p)
+        elif path.isdir(p):
+            shutil.rmtree(p)
+        else:
+            print 'File %s doesn\'t exists' % p
+            continue
+
+        # path to hidden path
+        ps.append(p.replace(path.join(HOME,'datasets'),path.join(HOME,'.datasets')))
+
     os.chdir(path.join(HOME,'.datasets'))
-    
-    assert not os.system(SVNRM % p),SVNRM % p
+    for p in ps:
+        assert not os.system(SVNRM % p),SVNRM % p
     assert not os.system(SVNCI)
 
 if __name__=="__main__":
