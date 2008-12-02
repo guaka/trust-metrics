116ae287b366a2fe921ddee88fc37b039b7876ce
the9ull <the9ull@0554d5a5-2f33-0410-a3cc-4d58a51ba057>
2008-12-02 10:14:45 +0000

    test

--- trustlet/netevolution.py
+++ trustlet/netevolution.py
@@ -254,7 +254,7 @@ def evolutionmap(load_path,functions,range=None,debug=None):
 
     #map list of result for each dataset in list of result for each function
     safe_merge(path.join(lpath,cachepath))
-    data_ordered = splittask(task,dates,notasksout=True)
+    data_ordered = splittask(task,dates,notasksout=True,np=1)
     safe_merge(path.join(lpath,cachepath))
     #data_ordered = [task(x) for x in dates]
     nd = len( dates )
