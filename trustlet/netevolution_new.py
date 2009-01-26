c96245957ab563f58d6915cb9d912f4c4be8d8a1
the9ull <the9ull@0554d5a5-2f33-0410-a3cc-4d58a51ba057>
2009-01-26 17:15:05 +0000

    resolved conflict

--- trustlet/netevolution_new.py
+++ trustlet/netevolution_new.py
@@ -423,6 +423,8 @@ def level_distribution(K,date):
                         K.level_map)))
 
     #order k from higher to lower values (Master to Observer)
+    assert K.level_map,K.level_map
+
     l = [d[k] for k,v in sorted(K.level_map.items(),lambda x,y: cmp(y[1],x[1])) if k and d[k]]
 
     return ( date, map(lambda x:1.0*x/sum(l),l))
