f5818b208651c355319d2a441fb390be86f1a9e7
the9ull <the9ull@0554d5a5-2f33-0410-a3cc-4d58a51ba057>
2009-05-28 12:55:41 +0000

    added syncrm

---
+++ scripts/syncrm.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os
+import optparse
+
+def remove(f):
+
+    print 'Removed',f
+    f = file(f,'w')
+    f.write('removed')
+    f.close()    
+
+def main():
+    
+    o = optparse.OptionParser(usage='usage: %prog [-r] [[[file1 [file2]] dir1] ...] ')
+    o.add_option('-r','--recursive',default=False,action='store_true')
+    
+    opts, args = o.parse_args()
+
+    for name in args:
+        if os.path.isdir(name) and opts.recursive:
+            for path,dirs,files in os.walk(name):
+                for file in files:
+                    remove(os.path.join(path,file))
+        if os.path.isfile(name):
+            remove(name,opts.recursive)
+
+main()
+
